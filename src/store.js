0cab198bb794977d241de1be9de408b8ab960336
Hugo Peres <hugo-pdf@hotmail.com>
2018-08-13 07:37:53 -0300

    update store dev tools

--- src/store.js
+++ src/store.js
@@ -7,12 +7,15 @@ const initialState = {};
 
 const middleware = [thunk];
 
+let reduxTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const devTools = process.env.NODE_ENV === 'development' ?  reduxTools : null
+
 const store = createStore(
     rootReducer,
     initialState,
     compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        applyMiddleware(...middleware),        
     )
 );
 
